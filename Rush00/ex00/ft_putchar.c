5f6bd95fed28b1bce900a4ab917bbe7fde2ddd7a
dtakara <dtakara@student.42sp.org.br>
2020-11-22 20:03:26 +0000

    rush00

--- Rush00/ex00/ft_putchar.c
+++ Rush00/ex00/ft_putchar.c
@@ -6,13 +6,11 @@
 /*   By: dtakara <dtakara@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/05 08:57:34 by vtouffet          #+#    #+#             */
-/*   Updated: 2020/11/22 14:41:44 by dtakara          ###   ########.fr       */
+/*   Updated: 2020/11/22 20:00:53 by dtakara          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-
-ssize_t write(int saida, const void *conteudo, size_t nbyte);
+#include <unistd.h>
 
 void	ft_putchar(char c)
 {
