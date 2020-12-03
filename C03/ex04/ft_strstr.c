1070a0337a148710ffe35c6c79d5f1d1aafc5660
dtakara <dtakara@student.42sp.org.br>
2020-12-03 09:35:12 +0000

    correções C02

--- C03/ex04/ft_strstr.c
+++ C03/ex04/ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: dtakara <dtakara@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/02 22:59:05 by dtakara           #+#    #+#             */
-/*   Updated: 2020/12/02 23:05:33 by dtakara          ###   ########.fr       */
+/*   Updated: 2020/12/03 02:02:08 by dtakara          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,26 @@ char    *ft_strstr(char *str, char *to_find)
     int i;
     int j;
 
-    if (to_find[0] == '\0')
-        return (str);
+    if (to_find[0] == '\0') //vai conferir se agulha é nula
+        return (str);	//se for nula, vai retornar o palheiro
     i = 0;
-    while (str[i] != '\0')
+    while (str[i] != '\0') //percorrer o palheiro
     {
         j = 0;
-        while (to_find[j] != '\0')
+        while (to_find[j] != '\0') //enquanto a agulha for diferente de nulo
         {
-            if (str[i + j] == to_find[j])
+            if (str[i + j] == to_find[j]) //se a posição que ele está no palheiro é igual ao conteúdo da posição da agulha i= posição do palheiro que ele começa a verificar e j = posição da agulha
             {
-                j++;
+                j++; //se for verdadeiro, ele soma 1 em j
             }
             else
-                break ;
+                break ; //se não, sai da função
         }
-        if (to_find[j] == '\0')
+        if (to_find[j] == '\0') //se chegou até aqui é pq acabou a string da agulha e foi totalmente encontrada no palheiro
         {
-            return (str + i);
+            return (str + i); //daí retorna o palheiro a partir do momento em que vc achou a agulha(i)
         }
-        i++;
+        i++; //saindo da função, ele adiciona 1 ao palheiro para verificar as demais caixinhas do próprio palheiro pra ver se dá match com a agulha
     }
     return (0);
 }
